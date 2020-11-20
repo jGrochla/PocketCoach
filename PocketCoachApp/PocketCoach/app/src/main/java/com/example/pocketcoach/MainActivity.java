ef2e5da3ff85c3128e657482a39236249a34a1d3
Nils Wagner <nilz.wagner@gmail.com>
2020-11-20 11:06:32 +0100

    einbinden des drop down menus in die main activity für den logout

--- PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
+++ PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.pocketcoach;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -13,6 +18,25 @@ public class MainActivity extends AppCompatActivity {
     TextView  textViewLogInStatus; // to show the Log in Status of the user
 
     private FirebaseAuth firebaseAuth; // to check if the user is logged in
+
+    // Include the drop down menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logoutmenu, menu);
+        return true;
+    }
+
+    // gives function to the log out menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       // DEBUGGING
+        Toast.makeText(getApplicationContext(),
+                "Button wurde gedrückt",
+                Toast.LENGTH_SHORT).show();
+        
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
