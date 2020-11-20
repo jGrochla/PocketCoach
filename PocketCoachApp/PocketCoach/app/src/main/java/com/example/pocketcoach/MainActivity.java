a850afdd54f45f45bd0ea70330e6703b87b44b45
Nils Wagner <nilz.wagner@gmail.com>
2020-11-20 17:22:09 +0100

    log in activity hinzugefügt und die kommunikarion zwisschen log in Activity und main Activity erzeugt

--- PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
+++ PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
@@ -15,28 +15,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView  textViewLogInStatus; // to show the Log in Status of the user
+    TextView  textViewLogInStatus;     // to show the Log in Status of the user
 
     private FirebaseAuth firebaseAuth; // to check if the user is logged in
 
-    // Include the drop down menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.logoutmenu, menu);
-        return true;
-    }
 
-    // gives function to the log out menu
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-       // DEBUGGING
-        Toast.makeText(getApplicationContext(),
-                "Button wurde gedrückt",
-                Toast.LENGTH_SHORT).show();
-        
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +32,49 @@ public class MainActivity extends AppCompatActivity {
 
         if(firebaseAuth.getCurrentUser() == null) {
             textViewLogInStatus.setText(getString(R.string.not_logged_in));
+
+            //forward user to login
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+
         }else {
             textViewLogInStatus.setText(getString(R.string.logged_in));
         }
+    }
+
+    /***********************************************************************************************
+     * ON CREATE OPTIONS MENU
+     * Includes the drop down menu
+     **********************************************************************************************/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logoutmenu, menu);
+        return true;
+    }
+
+    /***********************************************************************************************
+     * ON OPTION ITEM SELECTED
+     * gives a function to the menu
+     **********************************************************************************************/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(firebaseAuth.getCurrentUser() == null) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.log_out_info_negativ),
+                    Toast.LENGTH_SHORT).show();
+            //forward user to login
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.log_out_info_positiv),
+                    Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+            //forward user to login
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/SignInActivity.java
@@ -0,0 +1,191 @@
+package com.example.pocketcoach;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+    final static String TAG = "SignInActivity";
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    TextView textViewForgotPassword; // OnClickText to reset your password
+    EditText editTextEMail;          // Edit Text to put in the Mail Address
+    EditText editTextPassword;       // Edit Text to put in the Password
+    Button   buttonLogIn;            // Button for the Log In
+    Button   buttonRegister;         // Button to register
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        firebaseAuth = FirebaseAuth.getInstance(); // gets the log in status of the user
+
+        // initialize the Views from the Log In Activity
+        textViewForgotPassword = (TextView) findViewById(R.id.textViewForgotPassword);
+        editTextEMail          = (EditText) findViewById(R.id.editTextEmailAddress);
+        editTextPassword       = (EditText) findViewById(R.id.editTextPassword);
+        buttonLogIn            = (Button)   findViewById(R.id.buttonLogIn);
+        buttonRegister         = (Button)   findViewById(R.id.buttonRegister);
+
+        // add On Click Listener to the Buttons
+        buttonLogIn.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+
+        // impementation of the mAuthListener
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                // Query of the current user
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                    //Command to send the confirmation email
+                    user.sendEmailVerification().
+                            addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                // Logout of the user and notification that
+                                // the email has been sent.
+                                FirebaseAuth.getInstance().signOut();
+                                Toast.makeText(getApplicationContext(),
+                                        R.string.verify_mail,
+                                        Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(),
+                                        task.getException().getMessage(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        };
+    }
+
+    /***********************************************************************************************
+     * ON CLICK
+     * reacts when a button is clicked
+     * this method is called through the OnClickListener
+     **********************************************************************************************/
+    @Override
+    public void onClick(View view) {
+        String email = "";
+        String password = "";
+        switch (view.getId()) {
+            case R.id.buttonLogIn:
+                email = editTextEMail.getText().toString();
+                password = editTextPassword.getText().toString();
+                signIn(email, password);
+                break;
+            case R.id.buttonRegister:
+                email = editTextEMail.getText().toString();
+                password = editTextPassword.getText().toString();
+                registrate(email, password);
+                break;
+        }
+    }
+    /***********************************************************************************************
+     * REGISTER
+     * to register a new User
+     **********************************************************************************************/
+    private void registrate(String email, String password){
+        Log.d(TAG, "createAcccount: " + email);
+        if(!validateForm()) {
+            return;
+        }
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            mAuthListener.onAuthStateChanged(firebaseAuth);
+                        }else {
+                            Toast.makeText(getApplicationContext(),
+                                    task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    /***********************************************************************************************
+     * SIGN IN
+     * to sign in an already existing User
+     **********************************************************************************************/
+    private void signIn(String email, String password){
+        Log.d(TAG, "Log In: " + email);
+        if(!validateForm()){
+            return;
+        }
+        // sign In process
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //checks if E-Mail has been approved
+                            if(firebaseAuth.getCurrentUser().isEmailVerified()) {
+                                Toast.makeText(getApplicationContext(),
+                                        getString(R.string.successful_log_in),
+                                        Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(getApplicationContext(),
+                                        R.string.no_successful_log_in,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }else{
+
+                            Toast.makeText(getApplicationContext(),
+                                    task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+    }
+
+    /***********************************************************************************************
+     * VALIDATE FORM
+     * to check if the input of the Edit Text View is valid
+     **********************************************************************************************/
+    private boolean validateForm(){
+        boolean valid = true;
+
+        String email = editTextEMail.getText().toString();
+        if(email.isEmpty()){
+            editTextEMail.setError("Required.");
+            valid = false;
+        }else {
+            editTextEMail.setError(null);
+        }
+
+        String password = editTextPassword.getText().toString();
+        if(password.isEmpty()){
+            editTextPassword.setError("Required");
+            valid = false;
+        }else{
+            editTextPassword.setError(null);
+        }
+        return valid;
+    }
+
+}
